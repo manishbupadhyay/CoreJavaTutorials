a1d179fc0be251c58ffcbd1bfbafd7ccb994d20a
Manish Upadhyay <ermanishupadhyay@gmail.com>
2020-04-24 00:19:30 +0530

    Final vs Finally vs Finalize concept in Java
    Final vs Finally vs Finalize concept in Java
    
    Co-Authored-By: Manish Upadhyay <ermanishupadhyay@gmail.com>

---
+++ CoreJavaTutorials/src/OOPConceptPart2/FinalConcept.java
@@ -0,0 +1,15 @@
+package OOPConceptPart2;
+
+public class FinalConcept {
+
+	public static void main(String[] args) {
+		
+		final int i = 10; // final keyword is used to define constant value that we can not change value
+		//i = 20;
+		//i = 30;
+		
+		System.out.println(i);
+
+	}
+
+}
