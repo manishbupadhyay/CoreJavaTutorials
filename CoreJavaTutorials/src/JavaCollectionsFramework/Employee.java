da1e1e2066d8a2d8e04267736f4f711780adcb8e
Manish Upadhyay <ermanishupadhyay@gmail.com>
2020-06-28 01:31:04 +0530

    How to use HashMaps in Java || Hashmap in java with example program
    How to use HashMaps in Java || Hashmap in java with example program
    
    Co-Authored-By: Manish Upadhyay <ermanishupadhyay@gmail.com>

--- CoreJavaTutorials/src/JavaCollectionsFramework/Employee.java
+++ CoreJavaTutorials/src/JavaCollectionsFramework/Employee.java
@@ -2,12 +2,12 @@ package JavaCollectionsFramework;
 
 public class Employee {
 	
-	String name;
-	int age;
-	String dept;
+	public String name;
+	public int age;
+	public String dept;
 	
 	// constructor
-	Employee(String name,int age,String dept){
+	public Employee(String name,int age,String dept){
 		this.name = name;
 		this.age = age;
 		this.dept = dept;
